c69c8e0a6b068250345fbbb9d81ffa4e1cbc5d7f
AmeN <pawel.reclaw@gmail.com>
2021-10-30 14:12:45 +0200

    Final table version

--- src/components/AppContentDatabase.js
+++ src/components/AppContentDatabase.js
@@ -1,7 +1,41 @@
+import CardComponent from "./CardComponent";
+import classes from './AppContentDatabase.module.css';
+import {useState} from "react";
+import {DataGrid} from "@mui/x-data-grid";
+
+const EMPTY_OFFERS_LIST = [{
+    'id': 1,
+    'make': 'abc',
+    'model': 'def',
+    'economy': 5.0,
+    'body': 'SUV',
+    'price': 100.0
+}];
+
+const TABLE_MODEL = [
+    {width: 100, field: 'id', headerName: 'Id', sortable: false},
+    {width: 100, field: 'make', headerName: 'Make', sortable: false},
+    {width: 120, field: 'model', headerName: 'Model', sortable: false},
+    {width: 170, field: 'economy', headerName: 'Economy[1-10]', sortable: false},
+    {width: 140, field: 'body', headerName: 'Body/Type', sortable: false},
+    {width: 120, field: 'price', headerName: 'Price[$]', sortable: false},
+];
+
 const AppContentDatabase = () => {
+    const [offers, setOffers] = useState([...EMPTY_OFFERS_LIST]);
+
     return (
         <div>
-            Database
+            <CardComponent title={'Car Rental Offers'}>
+                <div className={classes.TableContainer}>
+                    <DataGrid
+                        rows={offers}
+                        columns={TABLE_MODEL}
+                        rowsPerPageOptions={[5]}
+                        pageSize={10}>
+                    </DataGrid>
+                </div>
+            </CardComponent>
         </div>
     )
 }
